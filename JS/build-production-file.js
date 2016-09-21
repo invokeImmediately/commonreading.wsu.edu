4608b2eb0f3260d7102913845fd3f1b494ac1e93
Daniel Rieck <danielcrieck@gmail.com>
2016-09-21 11:38:09 -0700

    Adding dirty form warning script to JS build

--- JS/build-production-file.js
+++ JS/build-production-file.js
@@ -2,7 +2,8 @@
 var concat = require('../../../../AppData/Roaming/npm/node_modules/concat-files');
 concat([
  './cr-custom.js',
- '../WSU-UE---JS/jQuery.forms.js'
+ '../WSU-UE---JS/jQuery.forms.js',
+ '../WSU-UE---JS/jQuery.are-you-sure.js'
  ], './wp-custom-js-source.js', function() {
     console.log('Concatenation complete.');     
  });
