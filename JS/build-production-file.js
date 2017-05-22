5bfed79e18db50ec0e2c1848f1bb7843a7f2ee2f
Daniel Rieck <danielcrieck@gmail.com>
2017-05-22 09:47:41 -0700

    Updating JS build order & dependencies

--- JS/build-production-file.js
+++ JS/build-production-file.js
@@ -1,10 +1,11 @@
 /* NODE.JS - Build Production JavaScript File */
 var concat = require('../../../../AppData/Roaming/npm/node_modules/concat-files');
 concat([
- './cr-custom.js',
+ '../WSU-UE---JS/jQuery.oue-custom.js',
  '../WSU-UE---JS/jQuery.forms.js',
- '../WSU-UE---JS/jQuery.are-you-sure.js'
+ '../WSU-UE---JS/jQuery.are-you-sure.js',
+ '../WSU-UE---JS/jQuery.masonry.min.js',
+ './cr-custom.js'
  ], './wp-custom-js-source.js', function() {
     console.log('Concatenation complete.');     
  });
- 
