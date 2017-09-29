07942f9aa073b7d2fdf236f6d81806e43f06d014
Daniel Rieck <danielcrieck@gmail.com>
2017-09-29 15:36:59 -0700

    Updating custom JS build
    Rebuilt production files to incorporate recent changes to source code and/or dependencies

--- JS/wp-custom-js-source.js
+++ JS/wp-custom-js-source.js
@@ -1727,85 +1727,118 @@ function processCountdownTimerMsg ( $countdownTimerMsg ) {
  *********************************************************************************************************************/
 "use strict";
 
-(function ($) {
-	$(function () {
+( function ( $ ) {
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// DOM LOADED:
+	// Call functions to be executed once the DOM is loaded.
+
+	$( function () {
  		var params = new Object();
 		var theseParams;
-		
+
+		// Setup parameters for function calls.
 		params.initCalendarItemExpansion = {
-			slctrCalendars: "ul.cascaded-layout.calendar.thirds",
-			expansionClass: "double",
+			slctrCalendars: 'ul.cascaded-layout.calendar.thirds',
+			expansionClass: 'double',
 			expansionDelay: 3000
 		};
-		
+
+		params.removeHiddenBlackKeyCluePosts = {
+			slctrPageTypes = '.blog, .archive.category',
+			slctrBlackKeyPosts = '.post.category-black-key-clue'
+		}
+
+		// Make function calls
 		theseParams = params.initCalendarItemExpansion;
 		initCalendarItemExpansion(
 			theseParams.slctrCalendars,
 			theseParams.expansionClass
 		);
-	});
-	
-	function initCalendarItemExpansion(slctrCalendars, expansionClass, expansionDelay) {
+
+		theseParams = params.removeHiddenBlackKeyCluePosts;
+		removeHiddenBlackKeyCluePosts(
+			theseParams.slctrPageTypes,
+			theseParams.slctrBlackKeyPosts
+		);
+	} );
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// FUNCTION DECLARATIONS
+
+	function initCalendarItemExpansion( slctrCalendars, expansionClass, expansionDelay ) {
 		//TODO: implement keyboard activation
-		var $calendars = $(slctrCalendars);
-		if ($calendars.length) {
-			$calendars.each(function () {
-				var $thisCalendar = $(this);
-				var $items = $thisCalendar.children("li");
-				$items.each(function() {
-					var $thisItem = $(this);
-					bindDragSafeClick($thisItem, function() {
-						toggleCalendarItemExpansion($thisCalendar, $thisItem, expansionClass, expansionDelay);
-					});
-				});
-			});
+		var $calendars = $( slctrCalendars );
+		if ( $calendars.length ) {
+			$calendars.each( function () {
+				var $thisCalendar = $( this );
+				var $items = $thisCalendar.children( 'li' );
+				$items.each( function () {
+					var $thisItem = $( this );
+					bindDragSafeClick( $thisItem, function() {
+						toggleCalendarItemExpansion( $thisCalendar, $thisItem, expansionClass, expansionDelay );
+					} );
+				} );
+			} );
 		}
 	}
-	
-	function toggleCalendarItemExpansion($calendar, $item, expansionClass, expansionDelay) {
-		$item.toggleClass(expansionClass);
+
+	function toggleCalendarItemExpansion( $calendar, $item, expansionClass, expansionDelay ) {
+		$item.toggleClass( expansionClass );
 		$calendar.masonry();
 	}
-	
-	function bindDragSafeClick($obj, callback) {
-		if($.isJQueryObj($obj)) {
-			$obj.mousedown(dragSafeMouseDown);
-			$obj.mousemove(dragSafeMouseMove);
-			$obj.mouseup(function () {
-				dragSafeMouseUp($obj, callback);
-			});
+
+	function bindDragSafeClick( $obj, callback ) {
+		if( $.isJQueryObj( $obj ) ) {
+			$obj.mousedown( dragSafeMouseDown );
+			$obj.mousemove( dragSafeMouseMove );
+			$obj.mouseup( function () {
+				dragSafeMouseUp( $obj, callback );
+			} );
 		}
 	}
-	
-	function dragSafeMouseDown(e) {
+
+	function dragSafeMouseDown( e ) {
 		var $this = $(this);
-		$this.data("wasDragging", 0);
-		$this.data("buttonClicked", e.button);
-		$this.data("dragSafeClickStart", {
+
+		$this.data( 'wasDragging', 0 );
+		$this.data( 'buttonClicked', e.button );
+		$this.data( 'dragSafeClickStart', {
 			clickX: e.pageX,
 			clickY: e.pageY
-		});
+		} );
 	}
-	
-	function dragSafeMouseMove(e) {
+
+	function dragSafeMouseMove( e ) {
 		var $this = $(this);
-		var clickStart = $this.data("dragSafeClickStart");
-		if (clickStart) {
-			var dx = Math.abs(clickStart.clickX - e.pageX);
-			var dy = Math.abs(clickStart.clickY - e.pageY);
-			if (dx > 4 || dy > 4) {
-				$this.data("wasDragging", 1);
+		var clickStart = $this.data( 'dragSafeClickStart' );
+		if ( clickStart ) {
+			var dx = Math.abs( clickStart.clickX - e.pageX );
+			var dy = Math.abs( clickStart.clickY - e.pageY );
+			if ( dx > 4 || dy > 4 ) {
+				$this.data( 'wasDragging' , 1);
 			}
 		}
 	}
 
-	function dragSafeMouseUp($this, clickCallback) {
-		if ($.isJQueryObj($this)) {
-			var buttonClicked = $this.data("buttonClicked");
-			var wasDragging = $this.data("wasDragging");
-			if (buttonClicked === 0 && !wasDragging) {
+	function dragSafeMouseUp( $this, clickCallback ) {
+		if ( $.isJQueryObj( $this ) ) {
+			var buttonClicked = $this.data( 'buttonClicked' );
+			var wasDragging = $this.data( 'wasDragging' );
+			if ( buttonClicked === 0 && !wasDragging ) {
 				clickCallback();
 			}			
 		}
 	}
-})(jQuery);
+
+	function removeHiddenBlackKeyCluePosts( slctrPageTypes, slctrBlackKeyPosts) {
+		var $pages = $( slctrPageTypes );
+		var $posts;
+
+		if ( $pages.length > 0 ) {
+			$posts = $pages.find( slctrBlackKeyPosts );
+			$posts.remove();
+		}
+	}
+
+} )( jQuery );
