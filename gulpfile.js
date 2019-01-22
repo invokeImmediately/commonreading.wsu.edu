ccd09d7ba8577ee1782b1cbe010ca03fa34ab689
Daniel Rieck, Ph.D <danielcrieck@gmail.com>
2019-01-21 21:54:45 -0800

    Modify function getJsBuildSettings
    Change dependencies for built custom JS file.

--- gulpfile.js
+++ gulpfile.js
@@ -55,11 +55,9 @@ function getJsBuildSettings() {
 			'../qTip2/dist/jquery.qtip.min.js',
 			'./WSU-UE---JS/jQuery.qTip.js',
 			'./WSU-UE---JS/jQuery.textResize.js',
-			'../imagesloaded/imagesloaded.pkgd.min.js',
-			'../masonry/dist/masonry.pkgd.min.js',
 			'./WSU-UE---JS/jQuery.masonry-custom.js',
-			'../jQuery.countdown/dist/jquery.countdown.min.js',
-			'./WSU-UE---JS/jQuery.countdown-custom.js',
+			// '../jQuery.countdown/dist/jquery.countdown.min.js',
+			// './WSU-UE---JS/jQuery.countdown-custom.js',
 			'./JS/cr-custom.js'
 		],
 		commentNeedle: /^(\/\*)(?!!)/g,
