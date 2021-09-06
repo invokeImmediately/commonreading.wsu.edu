8c30e7593e9870a8fbebf5d124765eefe8492740
invokeImmediately <danielcrieck@gmail.com>
2021-09-06 08:11:54 -0700

    Production: Update settings for custom CSS & JS builds
    In gulpfile.js, update references to CSS and JS submodule paths and the string for inserting inline documentation of the media query section to built stylesheets. Also, add function calls for setting up the help and default gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@
  *   custom CSS and JS code for designed for application to WSUWP website of the WSU Common Reading
  *   program.
  *
- * @version 1.0.0
+ * @version 1.0.1
  *
  * @link https://github.com/invokeImmediately/commonreading.wsu.edu/blob/master/gulpfile.js
  * @author Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
@@ -43,7 +43,7 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // §1: Gulp task dependencies
 
-var gulpBuilder = require( './WSU-UE---JS/gulpBuilder.js' );
+var gulpBuilder = require( './WSU-DAESA-JS/gulpCssJsBuilder.js' );
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // §2: Specificiation of build settings 
@@ -57,56 +57,20 @@ var gulpBuilder = require( './WSU-UE---JS/gulpBuilder.js' );
  * @return {object} - Instance of gulpBuilder.CssBuildSettings.
  */
 function getCssBuildSettings() {
-	var commentRemovalNeedle = /^(?:[ \t]*)?\/\*[^!].*$\n(?:^\*\*?[^/].*$\n)*\*\*?\/\n\n?/gm;
-	var dependenciesPath = './WSU-UE---CSS/';
-	var destFolder = './CSS/';
-	var fontImportStr = '@import url(\'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wg' +
-		'ht@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&family=PT+Serif:ital,wght@0,400;0,700' +
-		';1,400;1,700&family=Roboto+Condensed:ital,wght@0,400;0,700;1,400;1,700&family=Roboto+Mon' +
-		'o:ital,wght@0,400;0,700;1,400;1,700&display=swap\');\r\n';
-	var insertingMediaQuerySectionHeader = {
-			'before': /^@media/,
-			'lineBefore': '/*! ==================================================================' +
-				'==============================\r\n*** Media queries section\r\n*** =============' +
-				'================================================================================' +
-				'===\r\n***   SUMMARY: Media queries built from precompiled CSS written in the Le' +
-				'ss language extension of\r\n***    CSS. Queries in this section are a combinatio' +
-				'n of those designed for use on DAESA websites***\r\n    and those intended speci' +
-				'fically for use on the Common Reading program website.\r\n***\r\n***   DESCRIPTI' +
-				'ON: Fully documented, precompiled source code from which this section of the cus' +
-				'tom\r\n***    stylesheet was built is developed and maintained on the following ' +
-				'two GitHub projects:\r\n***    https://github.com/invokeImmediately/WSU-UE---CSS' +
-				'/\r\n***    https://github.com/invokeImmediately/commonreading.wsu.edu/\r\n***  ' +
-				' AUTHOR: Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediate' +
-				'ly)\r\n***\r\n***   LICENSE: ISC - Copyright (c) 2020 Daniel C. Rieck.\r\n***\r' +
-				'\n***     Permission to use, copy, modify, and/or distribute this software for a' +
-				'ny purpose with or\r\n***     without fee is hereby granted, provided that the a' +
-				'bove copyright notice and this permission\r\n***     notice appear in all copies' +
-				'.\r\n***\r\n***     THE SOFTWARE IS PROVIDED "AS IS" AND DANIEL C. RIECK DISCLAI' +
-				'MS ALL WARRANTIES WITH REGARD TO\r\n***     THIS SOFTWARE INCLUDING ALL IMPLIED ' +
-				'WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT\r\n***     SHALL DANIEL C' +
-				'. RIECK BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR' +
-				'\r\n***     ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, ' +
-				'WHETHER IN AN ACTION OF\r\n***     CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION' +
-				', ARISING OUT OF OR IN CONNECTION WITH THE USE\r\n***     OR PERFORMANCE OF THIS' +
-				' SOFTWARE.\r\n*** ==============================================================' +
-				'==================================\r\n**/',
-			'stopAfterFirstMatch': true
-		};
-	var minCssFileExtension = '.min.css';
-	var minCssFileHeaderStr = '';
-	var sourceFile = './CSS/cr-custom.less';
-
-	return new gulpBuilder.CssBuildSettings(
-		commentRemovalNeedle,
-		dependenciesPath,
-		destFolder,
-		fontImportStr,
-		insertingMediaQuerySectionHeader,
-		minCssFileExtension,
-		minCssFileHeaderStr,
-		sourceFile
-	);
+	return new gulpBuilder.CssBuildSettings( {
+		commentRemovalNeedle: /^(?:[ \t]*)?\/\*[^!].*$\n(?:^\*\*?[^/].*$\n)*\*\*?\/\n\n?/gm,
+		dependenciesPath: './WSU-DAESA-CSS/',
+		destFolder: './CSS/',
+		fontImportStr: gulpBuilder.getDaesaFontImportStr(),
+		insertingMediaQuerySectionHeader: {
+			before: /^@media/,
+			lineBefore: '/*! ================================================================================================\r\n *  ▐▀▄▀▌█▀▀▀ █▀▀▄ ▀█▀ ▄▀▀▄   ▄▀▀▄ █  █ █▀▀▀ █▀▀▄ ▀█▀ █▀▀▀ ▄▀▀▀\r\n *  █ ▀ ▌█▀▀  █  █  █  █▄▄█   █  █ █  █ █▀▀  █▄▄▀  █  █▀▀  ▀▀▀█\r\n *  █   ▀▀▀▀▀ ▀▀▀  ▀▀▀ █  ▀    ▀█▄  ▀▀  ▀▀▀▀ ▀  ▀▄▀▀▀ ▀▀▀▀ ▀▀▀\r\n *  ================================================================================================\r\n *  Media queries built from precompiled CSS written in the Less language extension of CSS. Queries\r\n *    in this section are a combination of those designed for use on all websites managed by WSU\r\n *    DAESA and those intended specifically for use on the Common Reading website.\r\n *\r\n *  Fully documented, precompiled source code from which this section of the stylesheet was\r\n *    developed is maintained on the following two GitHub projects:\r\n *      https://github.com/invokeImmediately/WSU-DAESA-CSS/\r\n *      https://github.com/invokeImmediately/commonreading.wsu.edu/\r\n *\r\n *  @author Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)\r\n *  @license: MIT - Copyright (c) 2021 Washington State University\r\n *    Permission is hereby granted, free of charge, to any person obtaining a copy of this software\r\n *      and associated documentation files (the "Software"), to deal in the Software without\r\n *      restriction, including without limitation the rights to use, copy, modify, merge, publish,\r\n *      distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom\r\n *      the Software is furnished to do so, subject to the following conditions:\r\n *    The above copyright notice and this permission notice shall be included in all copies or\r\n *      substantial portions of the Software.\r\n *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING\r\n *      BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND\r\n *      NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY\r\n *      CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,\r\n *      ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\r\n *      SOFTWARE.\r\n *  ================================================================================================\r\n */',
+			stopAfterFirstMatch: true
+		},
+		minCssFileExtension: '.min.css',
+		minCssFileHeaderStr: '',
+		sourceFile: './CSS/cr-custom.less'
+	} );
 }
 
 ////////
@@ -120,17 +84,17 @@ function getCssBuildSettings() {
 function getJsBuildSettings() {
 	return {
 		buildDependenciesList: [
-			'./WSU-UE---JS/jQuery.oue-custom.js',
-			'./WSU-UE---JS/jQuery.cookieObjs.js',
-			'./WSU-UE---JS/jQuery.forms.js',
+			'./WSU-DAESA-JS/jQuery.daesa-custom.js',
+			'./WSU-DAESA-JS/jQuery.cookieObjs.js',
+			'./WSU-DAESA-JS/jQuery.forms.js',
 			'../jQuery.AreYouSure/jquery.are-you-sure.js',
-			'./WSU-UE---JS/jQuery.are-you-sure.js',
+			'./WSU-DAESA-JS/jQuery.are-you-sure.js',
 			'../qTip2/dist/jquery.qtip.min.js',
-			'./WSU-UE---JS/jQuery.qTip.js',
-			'./WSU-UE---JS/jQuery.textResize.js',
-			'./WSU-UE---JS/jQuery.masonry-custom.js',
+			'./WSU-DAESA-JS/jQuery.qTip.js',
+			'./WSU-DAESA-JS/jQuery.textResize.js',
+			'./WSU-DAESA-JS/jQuery.masonry-custom.js',
 			// '../jQuery.countdown/dist/jquery.countdown.min.js',
-			// './WSU-UE---JS/jQuery.countdown-custom.js',
+			// './WSU-DAESA-JS/jQuery.countdown-custom.js',
 			'./JS/cr-custom.js'
 		],
 		commentNeedle: /^(\/\*)(?!!)/g,
@@ -146,5 +110,7 @@ function getJsBuildSettings() {
 
 gulpBuilder.setUpCssBuildTask( getCssBuildSettings() );
 gulpBuilder.setUpJsBuildTask( getJsBuildSettings() );
+gulpBuilder.setUpHelpTask();
+gulpBuilder.setUpDefaultTask();
 
 } )();
